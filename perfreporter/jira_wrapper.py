bc1141779c45f8c40f0a696fa64b0ee233479a36
Mykhailo Hunko <mykhailo_hunko@epam.com>
2019-09-06 14:53:58 +0300

    added JIRA wrapper, RP wrapper and reporter

---
+++ perfreporter/jira_wrapper.py
@@ -0,0 +1,221 @@
+from jira import JIRA
+import hashlib
+import io
+
+
+class JiraWrapper:
+    JIRA_REQUEST = 'project={} AND labels in ({})'
+
+    def __init__(self, args, url, user, password, jira_project, assignee, check_functional_errors,
+                 check_performance_degradation, check_missed_thresholds, performance_degradation_rate,
+                 missed_thresholds_rate, issue_type='Bug', labels=None, watchers=None, jira_epic_key=None):
+        self.valid = True
+        self.args = args
+        self.url = url
+        self.password = password
+        self.user = user
+        self.check_functional_errors = check_functional_errors
+        self.check_performance_degradation = check_performance_degradation
+        self.check_missed_thresholds = check_missed_thresholds
+        self.performance_degradation_rate = performance_degradation_rate
+        self.missed_thresholds_rate = missed_thresholds_rate
+        try:
+            self.connect()
+        except Exception as e:
+            self.valid = False
+            return
+        self.projects = [project.key for project in self.client.projects()]
+        self.project = jira_project
+        if self.project not in self.projects:
+            self.client.close()
+            self.valid = False
+            return
+        self.assignee = assignee
+        self.issue_type = issue_type
+        self.labels = list()
+        if labels:
+            self.labels = [label.strip() for label in labels.split(",")]
+        self.watchers = list()
+        if watchers:
+            self.watchers = [watcher.strip() for watcher in watchers.split(",")]
+        self.jira_epic_key = jira_epic_key
+        self.client.close()
+
+    def connect(self):
+        self.client = JIRA(self.url, basic_auth=(self.user, self.password))
+
+    def create_issue(self, title, priority, description, issue_hash, attachments=None, get_or_create=True,
+                     additional_labels=None):
+        _labels = [issue_hash]
+        if additional_labels and isinstance(additional_labels, list):
+            _labels.extend(additional_labels)
+        _labels.extend(self.labels)
+        issue_data = {
+            'project': {'key': self.project},
+            'summary': title,
+            'description': description,
+            'issuetype': {'name': self.issue_type},
+            'assignee': {'name': self.assignee},
+            'priority': {'name': priority},
+            'labels': _labels
+        }
+        jira_request = self.JIRA_REQUEST.format(issue_data["project"]["key"], issue_hash)
+        if get_or_create:
+            issue, created = self.get_or_create_issue(jira_request, issue_data)
+        else:
+            issue = self.post_issue(issue_data)
+            created = True
+        if attachments:
+            for attachment in attachments:
+                if 'binary_content' in attachment:
+                    self.add_attachment(issue.key,
+                                        attachment=attachment['binary_content'],
+                                        filename=attachment['message'])
+        for watcher in self.watchers:
+            self.client.add_watcher(issue.id, watcher)
+        if self.jira_epic_key:
+            self.client.add_issues_to_epic(self.jira_epic_key, [issue.id])
+        return issue, created
+
+    def add_attachment(self, issue_key, attachment, filename=None):
+        issue = self.client.issue(issue_key)
+        for _ in issue.fields.attachment:
+            if _.filename == filename:
+                return
+        self.client.add_attachment(issue, attachment, filename)
+
+    def post_issue(self, issue_data):
+        print(issue_data)
+        issue = self.client.create_issue(fields=issue_data)
+        return issue
+
+    def get_or_create_issue(self, search_string, issue_data):
+        issuetype = issue_data['issuetype']['name']
+        created = False
+        jira_results = self.client.search_issues(search_string)
+        issues = []
+        for each in jira_results:
+            if each.fields.summary == issue_data.get('summary', None):
+                issues.append(each)
+        if len(issues) == 1:
+            issue = issues[0]
+            if len(issues) > 1:
+                print('  more then 1 issue with the same summary')
+            else:
+                print(issuetype + 'issue already exists:' + issue.key)
+        else:
+            issue = self.post_issue(issue_data)
+            created = True
+        return issue, created
+
+    @staticmethod
+    def create_functional_error_description(error, arguments):
+        title = "Functional error in test: " + str(arguments['simulation']) + ". Request \"" \
+                + str(error['Request name']) + "\"."
+        description = "{panel:title=" + title + \
+                      "|borderStyle=solid|borderColor=#ccc|titleBGColor=#23b7c9|bgColor=#d7f0f3} \n"
+        description += "h3. Request description\n"
+        if error['Request name']:
+            description += "*Request name*: " + error['Request name'] + "\n"
+        if error['Method']:
+            description += "*HTTP Method*: " + error['Method'] + "\n"
+        if error['Request URL']:
+            description += "*Request URL*: " + error['Request URL'] + "\n"
+        if error['Request_params'] and str(error['Request_params']) != " ":
+            description += "*Request params*: " + str(error['Request_params'])[2:-2].replace(" ", "\n") + "\n"
+        if error['Request headers']:
+            description += "*Request headers*: {code}"
+            headers = str(error['Request headers']).replace(": ", ":")
+            for header in headers.split(" "):
+                description += header + "\n"
+            description += "{code}\n"
+        description += "---- \n h3. Error description\n"
+        if error['Error count']:
+            description += "*Error count*: " + str(error['Error count']) + ";\n"
+        if error['Response code']:
+            description += "*Response code*: " + error['Response code'] + ";\n"
+        if error['Error_message']:
+            description += "*Error message*: {color:red}" + str(error['Error_message']) + "{color}\n"
+        if error['Response']:
+            if len(str(error['Response'])) < 55000:
+                description += "---- \n h3. Response body {code:html}" + str(error['Response']) + "{code}"
+            else:
+                description += "---- \n See response body attached.\n"
+        description += "{panel}"
+        return description
+
+    @staticmethod
+    def get_functional_error_hash_code(error, arguments):
+        error_str = arguments['simulation'] + "_" + error['Request URL'] + "_" + str(error['Error_message']) + "_" \
+                    + error['Request name']
+        return hashlib.sha256(error_str.strip().encode('utf-8')).hexdigest()
+
+    @staticmethod
+    def create_performance_degradation_description(performance_degradation_rate, compare_with_baseline, arguments):
+        title = "Performance degradation in test: " + str(arguments['simulation'])
+        description = "{panel:title=" + title + \
+                      "|borderStyle=solid|borderColor=#ccc|titleBGColor=#23b7c9|bgColor=#d7f0f3} \n"
+        description += "{color:red}" + "Test performance degradation is {}% compared to the baseline."\
+            .format(performance_degradation_rate) + "{color} \n"
+        description += "h3. The following requests are slower than baseline:\n"
+        for request in compare_with_baseline:
+            description += "\"{}\" reached {} ms by {}. Baseline {} ms.\n".format(request['request_name'],
+                                                                                  request['response_time'],
+                                                                                  arguments['comparison_metric'],
+                                                                                  request['baseline'])
+        description += "{panel}"
+        return description
+
+    @staticmethod
+    def create_missed_thresholds_description(missed_threshold_rate, compare_with_thresholds, arguments):
+        title = "Missed thresholds in test: " + str(arguments['simulation'])
+        description = "{panel:title=" + title + \
+                      "|borderStyle=solid|borderColor=#ccc|titleBGColor=#23b7c9|bgColor=#d7f0f3} \n"
+        description += "{color:red}" + "Percentage of requests exceeding the threshold was {}%." \
+            .format(missed_threshold_rate) + "{color} \n"
+        description += "h3. The following requests exceeded the yellow threshold:\n"
+        for request in compare_with_thresholds:
+            if request['threshold'] == 'yellow':
+                description += "\"" + request['request_name'] + "\" reached " + str(request['response_time'])\
+                               + " ms by " + arguments['comparison_metric'] + ". Threshold - " + str(request['yellow'])\
+                               + " ms.\n"
+        description += "h3. The following requests exceeded the red threshold:\n"
+        for request in compare_with_thresholds:
+            if request['threshold'] == 'red':
+                description += "\"" + request['request_name'] + "\" reached " + str(request['response_time'])\
+                               + " ms by " + arguments['comparison_metric'] + ". Threshold - " + str(request['red'])\
+                               + " ms.\n"
+        description += "{panel}"
+        return description
+
+    def report_errors(self, aggregated_errors):
+        for error in aggregated_errors:
+            issue_hash = self.get_functional_error_hash_code(aggregated_errors[error], self.args)
+            title = "Functional error in test: " + str(self.args['simulation']) + ". Request \"" \
+                    + str(aggregated_errors[error]['Request name']) + "\" failed with error message: " \
+                    + str(aggregated_errors[error]['Error_message'])[0:100]
+            description = self.create_functional_error_description(aggregated_errors[error], self.args)
+            if len(str(aggregated_errors[error]['Response'])) < 55000:
+                self.create_issue(title, 'Major', description, issue_hash)
+            else:
+                content = io.StringIO()
+                content.write(str(aggregated_errors[error]['Response']))
+                attachment = {"binary_content": content, "message": "response_body.txt"}
+                self.create_issue(title, 'Major', description, issue_hash, [attachment])
+
+    def report_performance_degradation(self, performance_degradation_rate, compare_with_baseline):
+        issue_hash = hashlib.sha256("{} performance degradation".format(self.args['simulation']).strip()
+                                    .encode('utf-8')).hexdigest()
+        title = "Performance degradation in test: " + str(self.args['simulation'])
+        description = self.create_performance_degradation_description(performance_degradation_rate,
+                                                                      compare_with_baseline, self.args)
+        self.create_issue(title, 'Major', description, issue_hash)
+
+    def report_missed_thresholds(self, missed_threshold_rate, compare_with_thresholds):
+        issue_hash = hashlib.sha256("{} missed thresholds".format(self.args['simulation']).strip()
+                                    .encode('utf-8')).hexdigest()
+        title = "Missed thresholds in test: " + str(self.args['simulation'])
+        description = self.create_missed_thresholds_description(missed_threshold_rate,
+                                                                compare_with_thresholds, self.args)
+        self.create_issue(title, 'Major', description, issue_hash)
+
---
+++ perfreporter/reporter.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import logging
+import logging_loki
+import yaml
+
+from perfreporter.report_portal import ReportPortal
+from perfreporter.jira_wrapper import JiraWrapper
+
+
+PATH_TO_CONFIG = "/tmp/config.yaml"
+
+
+class Reporter(object):
+
+    @staticmethod
+    def parse_config_file(args):
+        report_types = []
+        with open(PATH_TO_CONFIG, "rb") as f:
+            config = yaml.load(f.read())
+        if config:
+            report_types = list(config.keys())
+
+        loki = None
+        if 'loki' in report_types:
+            loki_host = config['loki'].get("host")
+            loki_port = config['loki'].get("port")
+            if not all([loki_host, loki_port]):
+                print("Loki configuration values missing, proceeding "
+                      "without Loki")
+            else:
+                loki = "{}:{}/api/prom/push".format(loki_host, loki_port)
+
+        rp_service = None
+        if 'reportportal' in report_types:
+            rp_project = config['reportportal'].get("rp_project_name")
+            rp_url = config['reportportal'].get("rp_host")
+            rp_token = config['reportportal'].get("rp_token")
+            rp_launch_name = config['reportportal'].get("rp_launch_name")
+            check_functional_errors = config['reportportal'].get("check_functional_errors", 'False')
+            check_performance_degradation = config['reportportal'].get("check_performance_degradation", 'False')
+            check_missed_thresholds = config['reportportal'].get("check_missed_thresholds", 'False')
+            performance_degradation_rate = config['reportportal'].get("performance_degradation_rate", 20)
+            missed_thresholds_rate = config['reportportal'].get("missed_thresholds_rate", 50)
+            if not all([rp_project, rp_url, rp_token, rp_launch_name]):
+                print("ReportPortal configuration values missing, proceeding "
+                      "without report portal integration ")
+            else:
+                rp_service = ReportPortal(args, rp_url, rp_token, rp_project, rp_launch_name, check_functional_errors,
+                                          check_performance_degradation, check_missed_thresholds,
+                                          performance_degradation_rate, missed_thresholds_rate)
+                rp_service.my_error_handler(sys.exc_info())
+
+        jira_service = None
+        if 'jira' in report_types:
+            jira_url = config['jira'].get("url", None)
+            jira_user = config['jira'].get("username", None)
+            jira_pwd = config['jira'].get("password", None)
+            jira_project = config['jira'].get("jira_project", None)
+            jira_assignee = config['jira'].get("assignee", None)
+            jira_issue_type = config['jira'].get("issue_type", 'Bug')
+            jira_lables = config['jira'].get("labels", '')
+            jira_watchers = config['jira'].get("watchers", '')
+            jira_epic_key = config['jira'].get("epic_link", None)
+            check_functional_errors = config['jira'].get("check_functional_errors", 'False')
+            check_performance_degradation = config['jira'].get("check_performance_degradation", 'False')
+            check_missed_thresholds = config['jira'].get("check_missed_thresholds", 'False')
+            performance_degradation_rate = config['jira'].get("performance_degradation_rate", 20)
+            missed_thresholds_rate = config['jira'].get("missed_thresholds_rate", 50)
+            if not (jira_url and jira_user and jira_pwd and jira_project and jira_assignee):
+                print("Jira configuration values missing, proceeding without Jira")
+            else:
+                jira_service = JiraWrapper(args, jira_url, jira_user, jira_pwd, jira_project, jira_assignee,
+                                           check_functional_errors, check_performance_degradation, check_missed_thresholds,
+                                           performance_degradation_rate, missed_thresholds_rate, jira_issue_type,
+                                           jira_lables, jira_watchers, jira_epic_key)
+
+        return loki, rp_service, jira_service
+
+    @staticmethod
+    def report_errors(aggregated_errors, errors, args, loki, rp_service, jira_service):
+        if loki:
+            handler = logging_loki.LokiHandler(
+                url=loki,
+                tags={"Test": args['build_id']},
+            )
+            error_message = "Error key: {};; UTC Time: {};; Request name: {};; Method: {};; Response code: {};;" \
+                            " URL: {};; Error message: {};; Request params: {};; Headers: {};; Response body: {};;"
+            logger = logging.getLogger("error-logger")
+            logger.addHandler(handler)
+            for error in errors:
+                logger.error(
+                    error_message.format(str(error['error_key']), str(error['Time']), str(error['Request name']),
+                                         str(error['Method']), str(error['Response code']),
+                                         str(error['Request URL']), str(error['Error_message']),
+                                         str(error['Request_params']), str(error['Request headers']),
+                                         str(error['Response'])))
+
+        if rp_service and rp_service.check_functional_errors:
+            rp_service.report_errors(aggregated_errors)
+
+        if jira_service and jira_service.check_functional_errors:
+            jira_service.connect()
+            if jira_service.valid:
+                jira_service.report_errors(aggregated_errors)
+            else:
+                print("Failed connection to Jira or project does not exist")
+
+    @staticmethod
+    def report_performance_degradation(performance_degradation_rate, compare_with_baseline, rp_service, jira_service):
+        if rp_service and rp_service.check_performance_degradation:
+            rp_service.report_performance_degradation(performance_degradation_rate, compare_with_baseline)
+
+        if jira_service and jira_service.check_performance_degradation:
+            jira_service.connect()
+            if jira_service.valid:
+                if performance_degradation_rate > jira_service.performance_degradation_rate:
+                    jira_service.report_performance_degradation(performance_degradation_rate, compare_with_baseline)
+            else:
+                print("Failed connection to Jira or project does not exist")
+
+    @staticmethod
+    def report_missed_thresholds(missed_threshold_rate, compare_with_thresholds, rp_service, jira_service):
+        if rp_service and rp_service.check_missed_thresholds:
+            rp_service.report_missed_thresholds(missed_threshold_rate, compare_with_thresholds)
+
+        if jira_service and jira_service.check_missed_thresholds:
+            jira_service.connect()
+            if jira_service.valid:
+                if missed_threshold_rate > jira_service.missed_thresholds_rate:
+                    jira_service.report_missed_thresholds(missed_threshold_rate, compare_with_thresholds)
+            else:
+                print("Failed connection to Jira or project does not exist")
