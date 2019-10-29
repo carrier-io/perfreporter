620cc88a24cc09f8bbfe1549d3ab69e5f15f05ba
Mykhailo Hunko <mykhailo_hunko@epam.com>
2019-10-29 16:10:12 +0200

    update post-processing

--- perfreporter/data_manager.py
+++ perfreporter/data_manager.py
@@ -1,13 +1,7 @@
 from time import time
 import datetime
 from influxdb import InfluxDBClient
-
-SELECT_DATA_FROM_INFLUX = "SELECT last(tpsRate) as Throughput, min(responseTime) as Min, " \
-                                "max(responseTime) as Max, mean(responseTime) as Mean, " \
-                                "percentile(responseTime, 50) as pct_50, percentile(responseTime, 75) as pct_75, " \
-                                "percentile(responseTime, 90) as pct_90, percentile(responseTime, 95) as pct_95, " \
-                                "percentile(responseTime, 99) as pct_99 FROM requestsRaw WHERE " \
-                                "\"requestName\"=\'{}\' and buildID=\'{}\'"
+import numpy as np
 
 SELECT_LAST_BUILD_DATA = "select * from api_comparison where build_id=\'{}\'"
 
@@ -19,6 +13,11 @@ SELECT_BASELINE_DATA = "select * from api_comparison where build_id=\'{}\'"
 SELECT_THRESHOLDS = "select last(red) as red, last(yellow) as yellow from threshold where request_name=\'{}\' " \
                     "and simulation=\'{}\'"
 
+SELECT_USERS_COUNT = "select sum(\"distinct\") from (select distinct(\"user_count\") from \"users\" where " \
+                     "build_id='{}' group by lg_id)"
+
+SELECT_TEST_DATA = "select * from {} where build_id='{}'"
+
 
 class DataManager(object):
     def __init__(self, arguments):
@@ -27,72 +26,104 @@ class DataManager(object):
         self.client = InfluxDBClient(self.args["influx_host"], self.args['influx_port'],
                                      username=self.args['influx_user'], password=self.args['influx_password'])
 
-    def write_comparison_data_to_influx(self, test_results):
-        comparison = []
-        try:
-            self.client.switch_database(self.args['influx_database'])
-            for request in test_results:
-                data = self.client.query(SELECT_DATA_FROM_INFLUX.format(str(request['request_name']),
-                                                                        str(request['build_id'])))
-                data = list(data.get_points())[0]
-                request["throughput"] = round(float(data['Throughput']), 3)
-                request["min"] = round(float(data['Min']), 2)
-                request["max"] = round(float(data['Max']), 2)
-                request["mean"] = round(float(data['Mean']), 2)
-                request["pct50"] = int(data['pct_50'])
-                request["pct75"] = int(data['pct_75'])
-                request["pct90"] = int(data['pct_90'])
-                request["pct95"] = int(data['pct_95'])
-                request["pct99"] = int(data['pct_99'])
-                comparison.append(request)
-        except Exception as e:
-            print(e)
-
-        self.send_to_influx(comparison)
-
-    def send_to_influx(self, comparison):
-        points = []
+    def write_comparison_data_to_influx(self):
+        reqs = dict()
         timestamp = time()
-        for req in comparison:
+        user_count = self.get_user_count()
+
+        self.client.switch_database(self.args['influx_db'])
+        data = self.client.query(SELECT_TEST_DATA.format(self.args['simulation'], self.args['build_id']))
+        data = list(data.get_points())
+        start_time = int(str(datetime.datetime.strptime(data[0]['time'],
+                                                        "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()).split(".")[0]) \
+                     - int(int(data[0]['response_time']) / 1000)
+        end_time = int(str(datetime.datetime.strptime(data[len(data) - 1]['time'],
+                                                      "%Y-%m-%dT%H:%M:%S.%fZ").timestamp()).split(".")[0])
+        duration = end_time - start_time
+        for req in data:
+            key = '{} {}'.format(req["method"].upper(), req["request_name"])
+            if key not in reqs:
+                reqs[key] = {
+                    "times": [],
+                    "KO": 0,
+                    "OK": 0,
+                    "1xx": 0,
+                    "2xx": 0,
+                    "3xx": 0,
+                    "4xx": 0,
+                    "5xx": 0,
+                    'NaN': 0,
+                    "method": req["method"].upper(),
+                    "request_name": req['request_name']
+                }
+            reqs[key]['times'].append(int(req['response_time']))
+            if "{}xx".format(str(req['status_code'])[0]) in reqs[key]:
+                reqs[key]["{}xx".format(str(req['status_code'])[0])] += 1
+            else:
+                reqs[key]["NaN"] += 1
+            reqs[key][req['status']] += 1
+            reqs[key]['simulation'] = req['simulation']
+            reqs[key]['test_type'] = req['test_type']
+            reqs[key]['env'] = req['env']
+            reqs[key]['build_id'] = req['build_id']
+
+        if not reqs:
+            exit(0)
+        points = []
+        for req in reqs:
+            np_arr = np.array(reqs[req]["times"])
             influx_record = {
                 "measurement": "api_comparison",
                 "tags": {
-                    "simulation": req['simulation'],
-                    "users": req['users'],
-                    "test_type": req['test_type'],
-                    "build_id": req['build_id'],
-                    "request_name": req['request_name'],
-                    "method": req['method'],
-                    "duration": req['duration'],
+                    "simulation": reqs[req]['simulation'],
+                    "env": reqs[req]['env'],
+                    "users": user_count,
+                    "test_type": reqs[req]['test_type'],
+                    "build_id": reqs[req]['build_id'],
+                    "request_name": reqs[req]['request_name'],
+                    "method": reqs[req]['method'],
+                    "duration": int(self.args['end_time']) / 1000 - int(self.args['start_time']) / 1000
                 },
                 "time": datetime.datetime.fromtimestamp(timestamp).strftime('%Y-%m-%dT%H:%M:%SZ'),
                 "fields": {
-                    "throughput": req['throughput'],
-                    "total": req['total'],
-                    "ok": req['OK'],
-                    "ko": req['KO'],
-                    "1xx": req['1xx'],
-                    "2xx": req['2xx'],
-                    "3xx": req['3xx'],
-                    "4xx": req['4xx'],
-                    "5xx": req['5xx'],
-                    "NaN": req['NaN'],
-                    "min": req['min'],
-                    "max": req['max'],
-                    "mean": req['mean'],
-                    "pct50": req['pct50'],
-                    "pct75": req['pct75'],
-                    "pct90": req['pct90'],
-                    "pct95": req['pct95'],
-                    "pct99": req['pct99']
+                    "throughput": round(float(len(reqs[req]["times"])) / float(duration), 3),
+                    "total": len(reqs[req]["times"]),
+                    "ok": reqs[req]["OK"],
+                    "ko": reqs[req]["KO"],
+                    "1xx": reqs[req]["1xx"],
+                    "2xx": reqs[req]["2xx"],
+                    "3xx": reqs[req]["3xx"],
+                    "4xx": reqs[req]["4xx"],
+                    "5xx": reqs[req]["5xx"],
+                    "NaN": reqs[req]["NaN"],
+                    "min": round(np_arr.min(), 2),
+                    "max": round(np_arr.max(), 2),
+                    "mean": round(np_arr.mean(), 2),
+                    "pct50": int(np.percentile(np_arr, 50, interpolation="linear")),
+                    "pct75": int(np.percentile(np_arr, 75, interpolation="linear")),
+                    "pct90": int(np.percentile(np_arr, 90, interpolation="linear")),
+                    "pct95": int(np.percentile(np_arr, 95, interpolation="linear")),
+                    "pct99": int(np.percentile(np_arr, 99, interpolation="linear"))
                 }
             }
             points.append(influx_record)
         try:
-            self.client.switch_database(self.args['influx_comparison_database'])
+            self.client.switch_database(self.args['comparison_db'])
             self.client.write_points(points)
+            self.client.close()
         except Exception as e:
             print(e)
+            print("Failed connection to " + self.args["influx_host"] + ", database - comparison")
+
+    def get_user_count(self):
+        self.client.switch_database(self.args['influx_db'])
+        try:
+            data = self.client.query(SELECT_USERS_COUNT.format(self.args['build_id']))
+            data = list(data.get_points())[0]
+            return int(data['sum'])
+        except Exception as e:
+            print(e)
+        return 0
 
     def compare_with_baseline(self):
         baseline = self.get_baseline()
--- perfreporter/post_processor.py
+++ perfreporter/post_processor.py
@@ -11,14 +11,11 @@ class PostProcessor:
         self.config_file = config_file
         self.data_manager = DataManager(args)
 
-    def distributed_mode_post_processing(self):
-        self.data_manager.write_comparison_data_to_influx(self.comparison_data)
+    def post_processing(self):
+        self.data_manager.write_comparison_data_to_influx()
         if self.config_file:
             with open("/tmp/config.yaml", "w") as f:
                 f.write(self.config_file)
-        self.post_processing()
-
-    def post_processing(self):
         reporter = Reporter()
         loki, rp_service, jira_service = reporter.parse_config_file(self.args)
         performance_degradation_rate, compare_with_baseline = self.data_manager.compare_with_baseline()
